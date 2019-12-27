641da06b46070ba445b0f61cbcff8d89bc943ac3
mphil78 <philippe.marchand@gmail.com>
2019-12-27 15:51:52 +0100

    Revert "ORGA Deplacement manager dans le package"
    This reverts commit fd38c0eb26ab44f8652969ef8fb7d7bf16587da4.

--- src/fr/eni/eniencheres/bll/CategorieManager.java
+++ src/fr/eni/eniencheres/bll/CategorieManager.java
@@ -1,4 +1,4 @@
-package fr.eni.eniencheres.bll.managers;
+package fr.eni.eniencheres.bll;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -7,17 +7,17 @@ import fr.eni.eniencheres.bo.Categorie;
 import fr.eni.eniencheres.dal.CategorieDAO;
 import fr.eni.eniencheres.dal.DALException;
 import fr.eni.eniencheres.dal.DAOFactory;
+import fr.eni.eniencheres.dal.UtilisateurDAO;
 
 public class CategorieManager {
 
 	private CategorieDAO categorieDAO;
 
-	//constructeur
+
 	public CategorieManager() {
 		this.categorieDAO=DAOFactory.getCategorieDAO();
 	}
 	
-	// renvoie l'objet Catégorie par son libelle
 	public Categorie getByNom(String libelle) {
 		Categorie categorie=null;
 		try {
@@ -29,7 +29,6 @@ public class CategorieManager {
 		return categorie;
 	}
 
-	// renvoie une list de tous les libellés
 	public List<String> getAllLibelles() {
 		List<Categorie> listCategories = new ArrayList<>();
 		List<String> listeLibelles = new ArrayList<>();
@@ -42,6 +41,7 @@ public class CategorieManager {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		
 		return listeLibelles;
 	}
 }
