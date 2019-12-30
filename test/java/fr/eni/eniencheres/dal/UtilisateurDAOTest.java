fcf3429a2f24136d5c1bf7d312a52f99c78a9fa5
hugues <hugues@sony-vaio.home>
2019-12-30 01:21:48 +0100

    Validation des permiers tests unitaires avec maven et une base H2

--- test/java/fr/eni/eniencheres/dal/UtilisateurDAOTest.java
+++ test/java/fr/eni/eniencheres/dal/UtilisateurDAOTest.java
@@ -5,26 +5,35 @@ import fr.eni.eniencheres.dal.DAOFactory;
 import fr.eni.eniencheres.dal.UtilisateurDAO;
 import fr.eni.eniencheres.dal.DALException;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.Ignore;
 
 public class UtilisateurDAOTest {
+	
+	private static Logger LOG = Logger.getLogger("UtilisateurDAOTest");
 
     UtilisateurDAO utilisateurDAO = DAOFactory.getUtilisateurDAO();
 
     @Test
-    @Ignore
-    public void insertDifferentTest() throws DALException{
+    public void insertTest() throws DALException{
+    	int initialCount = utilisateurDAO.selectAll().size();
+    	LOG.log(Level.INFO, "before test<insertTest>, utilisateurs count :" + String.valueOf(initialCount));
         long id = System.currentTimeMillis();
-        Utilisateur u1 = new Utilisateur("titi" + String.valueOf(id), "DELAFONTAINE", "Robert", "tito@gmail.com",
+        Utilisateur u1 = new Utilisateur("titi" + String.valueOf(id), "Nom1", "Robert", "tito@gmail.com",
                 "0606060606", "12 rue du Corbeau et du Renard", "44444", "TDCDM", "motdepasse", 600, false);
         utilisateurDAO.insert(u1);
-        Utilisateur u2 = new Utilisateur("toto" + String.valueOf(id + 1), "DELAFONTAINE", "Robert", "tito@gmail.com",
+        Utilisateur u2 = new Utilisateur("toto" + String.valueOf(id + 1), "Nom2", "Robert", "tito@gmail.com",
                 "0606060606", "12 rue du Corbeau et du Renard", "44444", "TDCDM", "motdepasse", 600, false);
-        Utilisateur u3 = new Utilisateur("tutu" + String.valueOf(id + 2), "DELAFONTAINE", "Robert", "tito@gmail.com",
+        utilisateurDAO.insert(u2);
+        Utilisateur u3 = new Utilisateur("tutu" + String.valueOf(id + 2), "Nom3", "Robert", "tito@gmail.com",
                 "0606060606", "12 rue du Corbeau et du Renard", "44444", "TDCDM", "motdepasse", 600, true);
-
+        utilisateurDAO.insert(u3);
+        Assert.assertEquals("Test des insert",initialCount + 3,  utilisateurDAO.selectAll().size());
+        
         
     }
 
--- test/java/fr/eni/eniencheres/dal/jdbc/ConnectionProvider.java
+++ test/java/fr/eni/eniencheres/dal/jdbc/ConnectionProvider.java
@@ -1,57 +1,61 @@
 package fr.eni.eniencheres.dal.jdbc;
 
-
 import java.sql.Connection;
 import java.sql.SQLException;
-
-import java.sql.DriverManager;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import org.apache.tomcat.jdbc.pool.DataSource;
+import org.flywaydb.core.Flyway;
 
 abstract class ConnectionProvider {
 
-	private static Logger log = Logger.getLogger("ConnectionProvider");
+	private static Logger LOG = Logger.getLogger("ConnectionProvider");
 
-	private static Connection connection;
-	
-	static
-	{
-		try {
-			connection = DriverManager.getConnection("jdbc:hsqldb:file:./target/test-db", "sa", "");
-			
-        } catch (SQLException e) {
-            printSQLException(e);
-        }
-		
+	private static DataSource dataSource;
+	private static final String JDBC_URL_H2 = "jdbc:h2:file:./target/test-db;MODE=MSSQLSERVER";
+	private static final String JDBC_USER = "sa";
+	private static final String JDBC_PASSWORD = "";
+
+	static {
+
+		dataSource = new org.apache.tomcat.jdbc.pool.DataSource();
+		dataSource.setDriverClassName("org.h2.Driver");
+		dataSource.setUrl(JDBC_URL_H2);
+		dataSource.setUsername(JDBC_USER);
+		dataSource.setPassword(JDBC_PASSWORD);
+
+		Flyway flyway = Flyway.configure().dataSource(dataSource).load();
+		flyway.migrate();
+
 	}
 
 	public static void printSQLException(SQLException ex) {
-        for (Throwable e: ex) {
-            if (e instanceof SQLException) {
-                e.printStackTrace(System.err);
-                log.log(Level.SEVERE,"SQLState: " + ((SQLException) e).getSQLState());
-                log.log(Level.SEVERE,"Error Code: " + ((SQLException) e).getErrorCode());
-                log.log(Level.SEVERE,"Message: " + e.getMessage());
-                Throwable t = ex.getCause();
-                while (t != null) {
-                    log.log(Level.SEVERE,"Cause: " + t);
-                    t = t.getCause();
-                }
-            }
-        }
-    }
-	
+		for (Throwable e : ex) {
+			if (e instanceof SQLException) {
+				e.printStackTrace(System.err);
+				LOG.log(Level.SEVERE, "SQLState: " + ((SQLException) e).getSQLState());
+				LOG.log(Level.SEVERE, "Error Code: " + ((SQLException) e).getErrorCode());
+				LOG.log(Level.SEVERE, "Message: " + e.getMessage());
+				Throwable t = ex.getCause();
+				while (t != null) {
+					LOG.log(Level.SEVERE, "Cause: " + t);
+					t = t.getCause();
+				}
+			}
+		}
+	}
+
 	/**
-	 * Cette methode retourne une connection operationnelle issue du pool de connexion
-	 * vers la base de donnees. 
+	 * Cette methode retourne une connection operationnelle issue du pool de
+	 * connexion vers la base de donnees.
+	 * 
 	 * @return
 	 **/
 
-	public static Connection getConnection() throws SQLException
-	{
-		return connection;
+	public static Connection getConnection() throws SQLException {
+		LOG.log(Level.WARNING, "!!!  USE MOCK Connection with URL : " + JDBC_URL_H2);
+		return dataSource.getConnection();
 	}
 
 }
-
