26b9318ff0c365a59d4268b778c290e264c24c79
mphil78 <philippe.marchand@gmail.com>
2020-01-07 16:04:07 +0100

    Update EncheresManager.java

--- src/fr/eni/eniencheres/bll/EncheresManager.java
+++ src/fr/eni/eniencheres/bll/EncheresManager.java
@@ -1,8 +1,27 @@
 package fr.eni.eniencheres.bll;
 
+import fr.eni.eniencheres.bo.Enchere;
+import fr.eni.eniencheres.dal.DALException;
+import fr.eni.eniencheres.dal.EnchereDAO;
+import fr.eni.eniencheres.dal.RetraitDAO;
+
+
 public class EncheresManager {
+
 	
+	private EnchereDAO enchereDAO;
 	public final static int PADDING = 10;
+
+	public void addEnchere(Enchere enchere) {
+		
+		try {
+			enchereDAO.insert(enchere);
+		} catch (DALException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
 
 	
 	
